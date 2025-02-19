3dd194a2390d00f58cef5980d0df9bb6089743dd
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-02-19 12:11:10 +0100

    ...

--- src/components/first.js
+++ src/components/first.js
@@ -1,37 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/_first.scss";
 import Phone from "../assets/Icon-left.svg";
 import Arrow from "../assets/button-icon.svg";
 import Flag from "../assets/twemoji_flag-nigeria.svg";
 
 const First = () => {
+  const [languageOpen, setLanguageOpen] = useState(false);
+  const [countryOpen, setCountryOpen] = useState(false);
+
   return (
-    <div>
-      <main className="firstmain">
-        <div className="firstwhole">
-          <div className="firstleft">
-            <img className="phone" src={Phone} alt="phone" />{" "}
-            <number>+234 7065292789</number>
+    <main className="firstmain">
+      <div className="firstwhole">
+        <div className="firstleft">
+          <img className="phone" src={Phone} alt="phone" />
+          <number>+234 7065292789</number>
+        </div>
+
+        <div className="firstmiddle">
+          <span className="middle">Get 50% Off on Selected Items</span>
+          <a href="/" className="middle2">
+            <b>|</b> Shop Now
+          </a>
+        </div>
+
+        <div className="firstright">
+          <div className="dropdown">
+            <span 
+              className="language" 
+              onClick={() => setLanguageOpen(!languageOpen)}
+            >
+              EN <img className={`arrow ${languageOpen ? "rotate" : ""}`} src={Arrow} alt="arrow" />
+            </span>
+            {languageOpen && (
+              <ul className="dropdown-menu language-menu">
+                <li>English</li>
+                <li>French</li>
+                <li>Spanish</li>
+              </ul>
+            )}
           </div>
-          <div className="firstmiddle">
-            <span className="middle">Get 50% Off on Selected Items</span>
-            <span to="/" className="middle2">
-              {" "}
-              <b>|</b> Shop Now
+
+          {/* Country Dropdown */}
+          <div className="dropdown">
+            <span 
+              className="country" 
+              onClick={() => setCountryOpen(!countryOpen)}
+            >
+              <img className="flag" src={Flag} alt="flag" /> NIGERIA
+              <img className={`arrow ${countryOpen ? "rotate" : ""}`} src={Arrow} alt="arrow" />
             </span>
-          </div>
-          <div className="firstright">
-            <span className="language">
-              EN <img className="arrow" src={Arrow} alt="arrow" />{" "}
-            </span>
-            <span className="country">
-              <img className="flag" src={Flag} alt="arrow" /> NIGERIA{" "}
-              <img className="arrow" src={Arrow} alt="arrow" />{" "}
-            </span>
+            {countryOpen && (
+              <ul className="dropdown-menu country-menu">
+                <li>🇳🇬 Nigeria</li>
+                <li>🇺🇸 USA</li>
+                <li>🇬🇧 UK</li>
+              </ul>
+            )}
           </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 };
 
