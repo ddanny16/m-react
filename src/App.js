d7031959afdc97fe0d49b864f8dd88e71db98eac
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-02-19 14:28:20 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import First from "./components/first"
+import React from "react";
+import First from "./components/first";
+import Header from "./components/header";
 
 function App() {
   return (
     <div>
       <First />
+      <Header />
     </div>
   );
 }
