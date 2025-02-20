c8a603aafbca3e515794b62fbb80e10f124a304c
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2025-02-20 10:57:30 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import First from "./components/first";
 import Header from "./components/header";
+import Hero from "./components/hero"
 
 function App() {
   return (
     <div>
       <First />
       <Header />
+      <Hero />
+
     </div>
   );
 }
